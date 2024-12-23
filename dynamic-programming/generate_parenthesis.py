26a3ee669ba26edf113e7cf60d132c97a1a7e4bf
Bruno Sampaio <bruno.sampaio@ubisoft.com>
2024-12-23 14:04:00 -0500

    Set property inside test class

--- dynamic-programming/generate_parenthesis.py
+++ dynamic-programming/generate_parenthesis.py
@@ -5,25 +5,6 @@ Description: https://leetcode.com/problems/generate-parentheses
 """
 
 
-class TestSolution(unittest.TestCase):
-
-    def test_cases(self):
-        solution = Solution()
-
-        cases = [
-            (1, ['()']),
-            (3, ['((()))', '(()())', '(())()', '()(())', '()()()'])
-        ]
-
-        for case in cases:
-            n, expected = case
-
-            result = solution.solve(n)
-
-            for exp in expected:
-                self.assertIn(exp, result)
-
-
 class Solution:
     def do_it(self, n_open, n_close, str="", results=[]):
         if n_open == 0 and n_close == 0:
@@ -43,6 +24,24 @@ class Solution:
         self.do_it(n, n, "", results)
 
         return results
+
+
+class TestSolution(unittest.TestCase):
+    solution = Solution()
+
+    def test_cases(self):
+        cases = [
+            (1, ['()']),
+            (3, ['((()))', '(()())', '(())()', '()(())', '()()()'])
+        ]
+
+        for case in cases:
+            n, expected = case
+
+            result = self.solution.solve(n)
+
+            for exp in expected:
+                self.assertIn(exp, result)
 
 
 if __name__ == "__main__":
